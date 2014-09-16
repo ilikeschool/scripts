535f2819c8fd1a4bebfa61ca61d1a9f7b40f2096
ilikeschool <ilikeschool+github@gmail.com>
2014-09-16 14:32:30 -0700

    Added support functions for dnl and inl

--- python/mathfunc.py
+++ python/mathfunc.py
@@ -9,7 +9,7 @@ import numpy
 # Returns inl - Normalized INL data
 #         dnl - Normalized DNL data
 ###############################################################################
-def calcInlDnl(xDataIn,yDataIn):
+def calcInlDnl_bestfit(xDataIn,yDataIn):
     dnl = []
     midX = len(xDataIn)/2
     coeffs = numpy.polyfit(xDataIn,yDataIn,1)
@@ -20,5 +20,21 @@ def calcInlDnl(xDataIn,yDataIn):
         dnl.append((yDataIn[x]-yDataIn[x-1])/lsb-1)
     return inl,numpy.array(dnl)
 
+def calcInlDnl_endpoint(xDataIn,yDataIn):
+    dnl = []
+    lsb = (yDataIn[-1] - yDataIn[0])/(xDataIn[-1] - xDataIn[0])
+    offset = yDataIn[0]-xDataIn[0]*lsb
+    endpoint = [x*lsb + offset for x in xDataIn]
+    inl = numpy.subtract(yDataIn,endpoint)/lsb
+    for x in range(1,len(yDataIn)):
+        dnl.append((yDataIn[x]-yDataIn[x-1])/lsb-1)
+    return inl,numpy.array(dnl)
 
+def calcInlDnl_ideal(xDataIn,yDataIn,lsb):
+    dnl = []
+    ideal = [x*lsb for x in xDataIn]
+    inl = numpy.subtract(yDataIn,ideal)/lsb
+    for x in range(1,len(yDataIn)):
+        dnl.append((yDataIn[x]-yDataIn[x-1])/lsb-1)
+    return inl,numpy.array(dnl)
 
