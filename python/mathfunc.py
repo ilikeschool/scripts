cc7b364ca1008bfe0975f60c33aa9bb40ef57551
ilikeschool <ilikeschool+github@gmail.com>
2014-10-24 11:15:57 -0700

    Linking old indldnl function to bestfit version

--- python/mathfunc.py
+++ python/mathfunc.py
@@ -37,3 +37,7 @@ def calcInlDnl_ideal(xDataIn,yDataIn,lsb):
     for x in range(1,len(yDataIn)):
         dnl.append((yDataIn[x]-yDataIn[x-1])/lsb-1)
     return inl,numpy.array(dnl)
+
+
+def calcInlDnl(xDataIn,yDataIn):
+    return calcInlDnl_bestfit(xDataIn,yDataIn)
