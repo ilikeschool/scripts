663b73d5d18918141a76cb685920828cafe564c3
ilikeschool <ilikeschool+github@gmail.com>
2014-12-05 16:11:19 -0800

    Preamp noise now iterates over all combinations and outputs to csv.

--- python/preamp_noise.py
+++ python/preamp_noise.py
@@ -2,11 +2,21 @@
 import numpy as np
 import preamp_noise_funcs as nf
 import matplotlib.pyplot as plt
+import csv
 from matplotlib.backends.backend_pdf import PdfPages
 
-Rtot = 20e3
+fileName_sum_csv = "preamp_noise_summary.csv"
+fileName_scat_csv = "preamp_noise_scatter.csv"
+
+Rtot = 12e3
 gainSteps = np.arange(0,21)
-opNoiseScale = np.logspace(0,5,num=6,base=2)
+op1NoiseScales = np.linspace(1,4,4) 
+op2NoiseScales = np.linspace(1,4,4) 
+adcNoiseScales = np.linspace(1,3,3)
+micNoiseScales = np.linspace(0,1,2)
+opNoise = 2.12e-6
+adcNoise = 9.162e-6
+micNoise = 2.5e-6
 noiseList = ['OP1','RS','RF','OP2','RNS','RNF','RNBIAS','RAAP','RAAN','ADC','MIC']
 noiseTypes = ['op','res','res','op','res','res','res','res','res','adc','mic']
 noiseFuncs = [nf.calcNonInvGain,   ## OP1
@@ -29,19 +39,19 @@ noiseScales = [2,               ## OP1
         0,                      ## RNBIAS
         1,                      ## RAAP
         1,                      ## RAAN
-        1/np.sqrt(4),                      ## ADC
+        1,                      ## ADC
         0]                      ## MIC
-noiseInps = [3e-6,              ## OP1
+noiseInps = [opNoise,           ## OP1
         Rtot,                   ## RS
         0,                      ## RF
-        3e-6,                   ## OP2
+        opNoise,                ## OP2
         2e3,                    ## RNS
         2e3,                    ## RNF
         10e3,                   ## RNBIAS
         2e3,                    ## RAAP
         2e3,                    ## RAAN
-        3.53e-6,                ## ADC
-        2.5e-6]                 ## MIC
+        adcNoise,               ## ADC
+        micNoise]               ## MIC
 
 noiseDict = {}
 for noiseParam,noiseType,noiseFunc,noiseScale,noiseInp in zip(noiseList,noiseTypes,noiseFuncs,noiseScales,noiseInps):
@@ -52,8 +62,7 @@ for noiseParam,noiseType,noiseFunc,noiseScale,noiseInp in zip(noiseList,noiseTyp
 
 noiseDict['gain'] = 2*nf.calcNonInvGain(noiseDict['RF','input'],noiseDict['RS','input'])
 
-'''
-gain = []
+
 compNoise = []
 opCompNoise = []
 resCompNoise = []
@@ -63,70 +72,103 @@ opContr = []
 resContr = []
 adcContr = []
 micContr = []
+dataArray = []
+dataDictList = [dict() for x in gainSteps]
 
+for micNoiseScale in micNoiseScales:
+    for adcNoiseScale in adcNoiseScales:
+        for op1NoiseScale in op1NoiseScales:
+            for op2NoiseScale in op2NoiseScales:
+                for gainStep in gainSteps:
+                    Rstep = (1-10**(-gainStep/20.0))*Rtot
+                    noiseDict['RF','input'] = Rstep
+                    noiseDict['RS','input'] = Rtot-Rstep
+                    noiseDict['gain'] = 2*nf.calcNonInvGain(noiseDict['RF','input'],noiseDict['RS','input'])
+                    noiseDict['OP1','input'] = opNoise/np.sqrt(op1NoiseScale)
+                    noiseDict['OP2','input'] = opNoise/np.sqrt(op2NoiseScale)
+                    noiseDict['ADC','input'] = adcNoise/np.sqrt(2**(adcNoiseScale-1))
+                    noiseDict['MIC','scale'] = micNoiseScale*2
+                    cNoise,oNoise,rNoise,aNoise,mNoise = nf.calcPreampNoise(noiseList,noiseDict)
+                    compNoise = cNoise*1e6
+                    opCompNoise = oNoise*1e6
+                    resCompNoise = rNoise*1e6
+                    adcCompNoise = aNoise*1e6
+                    micCompNoise = mNoise*1e6
+                    opFilt = 1 if op1NoiseScale >= op2NoiseScale else 0
+                    fullScale = 10**(-gainStep/20.0)*1.0/np.sqrt(2)/2*1000
+                    snr = 20*np.log10(fullScale/compNoise*1000)
+                    snr_1vrms = 20*np.log10(1/cNoise)
+                    current = 50+20*(op1NoiseScale+op2NoiseScale)+75*2**(adcNoiseScale-1)
+                    fom = snr+10*np.log10(1/(3*current*1e-6))
+                    dataLabel = str(int(adcNoiseScale)) + "ADC/" + str(int(op1NoiseScale)) + "OP1/" + str(int(op2NoiseScale)) + "OP2"
+                    dataArray.append([micNoiseScale,adcNoiseScale,op1NoiseScale,op2NoiseScale,opFilt,gainStep,fullScale,compNoise,opCompNoise,resCompNoise,adcCompNoise,micCompNoise,snr,snr_1vrms,current,fom,dataLabel])
+                    if micNoiseScale == 0 and opFilt:
+                        key = (current)
+                        if key not in dataDictList[gainStep].keys():
+                            dataDictList[gainStep][key] = [compNoise,dataLabel]
+                        elif compNoise < dataDictList[gainStep][key][0]:
+                            dataDictList[gainStep][key] = [compNoise,dataLabel]
 
-for gainStep in gainSteps:
-    Rstep = (1-10**(-gainStep/20.0))*Rtot
-    noiseDict['RF','input'] = Rstep
-    noiseDict['RS','input'] = Rtot-Rstep
-    noiseDict['gain'] = 2*nf.calcNonInvGain(noiseDict['RF','input'],noiseDict['RS','input'])
-    cNoise,oNoise,rNoise,aNoise,mNoise = nf.calcPreampNoise(noiseList,noiseDict)
-    gain.append(noiseDict['gain'])
-    compNoise.append(cNoise*1e6)
-    opCompNoise.append(oNoise*1e6)
-    opContr.append(oNoise**2/cNoise**2*100)
-    resCompNoise.append(rNoise*1e6)
-    resContr.append(rNoise**2/cNoise**2*100)
-    adcCompNoise.append(aNoise*1e6)
-    adcContr.append(aNoise**2/cNoise**2*100)
-    micCompNoise.append(mNoise*1e6)
-    micContr.append(mNoise**2/cNoise**2*100)
+with open(fileName_sum_csv,'wb') as f:
+    writer= csv.writer(f)
+    header = ['MIC','ADC','OP1','OP2','OPFILT','GAIN','FS','COMPN','OPN','RESN','ADCN','MICN','SNR','SNR_1Vrms','CURRENT','FOM','LABEL']
+    writer.writerow(header)
+    for data in dataArray:
+        writer.writerow(data)
 
-compNoiseArray = [np.array(compNoise),np.array(opCompNoise),np.array(resCompNoise),np.array(adcCompNoise),np.array(micCompNoise)]
-contrArray = [np.array(opContr),np.array(resContr),np.array(adcContr),np.array(micContr)]
-legText = ['Total','Opamp','Resistor','ADC','Microphone']
-plotDict = {}
+minNoise = [float("inf") for x in gainSteps]
+with open(fileName_scat_csv,'wb') as f:
+    writer= csv.writer(f)
+    header = np.concatenate((['current'],gainSteps,gainSteps))
+    writer.writerow(header)
+    dataKeys = sorted(dataDictList[0])
+    for key in dataKeys:
+        rowNoise = []
+        rowLabel = []
+        for gainStep in gainSteps:
+            if dataDictList[gainStep][key][0] < minNoise[gainStep]:
+                minNoise[gainStep] =  dataDictList[gainStep][key][0]
+                rowNoise = rowNoise + [dataDictList[gainStep][key][0]]
+                rowLabel = rowLabel + [dataDictList[gainStep][key][1]]
+            else:
+                rowNoise = rowNoise + [''] 
+                rowLabel = rowLabel + ['']
+                
+        row = [key] + rowNoise + rowLabel
+        writer.writerow(row)
 
-with PdfPages('gain_sweep.pdf') as pp:
-    fig = nf.plotPreampNoise(gainSteps,compNoiseArray,contrArray)
-    pp.savefig()
-    plt.close('all')
-'''
-compNoise = []
-opCompNoise = []
-resCompNoise = []
-adcCompNoise = []
-micCompNoise = []
-opContr = []
-resContr = []
-adcContr = []
-micContr = []
-
-opNoise = noiseDict['OP1','input']
-for opStep in opNoiseScale:
-    noiseDict['OP1','input'] = opNoise/np.sqrt(opStep)
-    noiseDict['OP2','input'] = opNoise/np.sqrt(opStep)
-    cNoise,oNoise,rNoise,aNoise,mNoise = nf.calcPreampNoise(noiseList,noiseDict)
-    compNoise.append(cNoise*1e6)
-    opCompNoise.append(oNoise*1e6)
-    opContr.append(oNoise**2/cNoise**2*100)
-    resCompNoise.append(rNoise*1e6)
-    resContr.append(rNoise**2/cNoise**2*100)
-    adcCompNoise.append(aNoise*1e6)
-    adcContr.append(aNoise**2/cNoise**2*100)
-    micCompNoise.append(mNoise*1e6)
-    micContr.append(mNoise**2/cNoise**2*100)
-
-
-compNoiseArray = [np.array(compNoise),np.array(opCompNoise),np.array(resCompNoise),np.array(adcCompNoise),np.array(micCompNoise)]
-contrArray = [np.array(opContr),np.array(resContr),np.array(adcContr),np.array(micContr)]
-legText = ['Total','Opamp','Resistor','ADC','Microphone']
-plotDict = {}
-
-with PdfPages('opNoise_sweep_adc_4x.pdf') as pp:
-    fig = nf.plotPreampNoise(opNoiseScale,compNoiseArray,contrArray)
-    pp.savefig()
-    plt.close('all')
-
-
-
+#compNoise = []
+#opCompNoise = []
+#resCompNoise = []
+#adcCompNoise = []
+#micCompNoise = []
+#opContr = []
+#resContr = []
+#adcContr = []
+#micContr = []
+#
+#opNoise = noiseDict['OP1','input']
+#for opStep in opNoiseScale:
+#    noiseDict['OP1','input'] = opNoise/np.sqrt(opStep)
+#    noiseDict['OP2','input'] = opNoise/np.sqrt(opStep)
+#    cNoise,oNoise,rNoise,aNoise,mNoise = nf.calcPreampNoise(noiseList,noiseDict)
+#    compNoise.append(cNoise*1e6)
+#    opCompNoise.append(oNoise*1e6)
+#    opContr.append(oNoise**2/cNoise**2*100)
+#    resCompNoise.append(rNoise*1e6)
+#    resContr.append(rNoise**2/cNoise**2*100)
+#    adcCompNoise.append(aNoise*1e6)
+#    adcContr.append(aNoise**2/cNoise**2*100)
+#    micCompNoise.append(mNoise*1e6)
+#    micContr.append(mNoise**2/cNoise**2*100)
+#
+#
+#compNoiseArray = [np.array(compNoise),np.array(opCompNoise),np.array(resCompNoise),np.array(adcCompNoise),np.array(micCompNoise)]
+#contrArray = [np.array(opContr),np.array(resContr),np.array(adcContr),np.array(micContr)]
+#legText = ['Total','Opamp','Resistor','ADC','Microphone']
+#plotDict = {}
+#
+#with PdfPages('opNoise_sweep_adc_4x.pdf') as pp:
+#    fig = nf.plotPreampNoise(opNoiseScale,compNoiseArray,contrArray)
+#    pp.savefig()
+#    plt.close('all')
